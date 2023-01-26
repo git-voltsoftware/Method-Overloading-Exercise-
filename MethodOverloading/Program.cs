f05d05076798e0e3f2f7bb07ff34e4a40b0555cd
git-voltsoftware <info@volt.software>
2023-01-26 13:48:13 -0500

    method overloading

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,56 @@
 ﻿namespace MethodOverloading
 {
-    public class Program
+    class Program
     {
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool isCheck)
+        {
+            var sum = a + b;
+
+            if (isCheck == true && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (isCheck == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else if (isCheck == true && sum < 1)
+            {
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var x = 29;
+            var y = 55;
+
+            var answer = Add(x, y);
+
+            var a = 62.0;
+            var b = 55.0;
+
+            var decimalAnswer = Add(a, b);
+
+            var thirdAnswer = Add(0, 0, true);
+
+            Console.WriteLine($"int add: {answer} decimal add: {decimalAnswer}");
+            Console.WriteLine(thirdAnswer);
         }
     }
 }
